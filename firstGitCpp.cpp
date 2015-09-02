2f5f9b516c41c290e58e40921013bd2dd7328e41
Prasad Pawar <ubuntu.prasad@gmail.com>
2015-09-02 18:51:01 +0530

    Added first git file

---
+++ firstGitCpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int i;
+
+  cout << "==============================================================" <<endl;
+  cout << "Hello Git....This is my first github program using Cpp language" <<endl;
+  cout << "==============================================================" <<endl;
+
+  for(i = 0; i <= 10; ++i)
+    cout << i <<"->" << pow(i,3) <<endl;
+
+  cout << "==============================================================";
+
+return 0;
+
+}
